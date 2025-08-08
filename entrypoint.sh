4e0d193fa0f1ae79731f6e39954c56936e3559ce
Daniel Wee <danielweedk@gmail.com>
2025-08-08 19:10:13 +0800

    Updated deployment stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 touch db.sqlite
-# exec uv run fastapi run main.py --host 0.0.0.0 --port 5046
-exec uv run fastapi dev main.py --host 0.0.0.0 --port 5046
+exec uv run fastapi run main.py --host 0.0.0.0 --port 5046
+# exec uv run fastapi dev main.py --host 0.0.0.0 --port 5046
